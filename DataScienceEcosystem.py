1e6896fbb39550d7972bb8d71a062518046b0cf6
Abraham Cedeño Levy <110000650+abraham-cedeno@users.noreply.github.com>
2023-09-12 08:27:08 -0500

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Some of the popular languages that Data Scientists use are:
+# 
+#     1. Python
+#     2. R
+#     3. Julia
+#     4. Java
+#     5. C ++
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. Caret
+# 2. Pandas
+# 3. NumPY
+# 4. ggplot2
+# 5. Scikit-learn
+# 6. TensorFlow
+
+# The subsequent three rows in the table should indicate three development environment open source tools used in data science.
+# 
+# |Development Enfironment's for DS |
+# |---------------------------------|
+# |            GitHub               |
+# |              Rstudio                |
+# |            Jupyter Notebooks/Jupyter lab            |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[4]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[5]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+
+minutes = 200
+hours = minutes / 60
+print(f"{hours:.1f} hours")
+
+
+# **Objectives:**
+# 
+# * 1. Learn the most used Data Science languages.
+# * 2. Learn the most used Libraries in Data Science.
+# * 3. Learn about the most used development environments in Data Science.
+# * 4. Learn how to use jupyter Notebooks.
+# * 5. Learn basic python code to perform some calculations.
+
+# ## Author
+# Abraham Cedeño
